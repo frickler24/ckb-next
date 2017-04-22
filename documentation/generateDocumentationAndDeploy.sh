f47a204629b0310c12d4c70ab2b9588d721f0c24
Lutz Eichler <github@frickler24.de>
2017-04-22 22:11:17 +0200

    debug documentation/generateDocumentationAndDeploy.sh

--- documentation/generateDocumentationAndDeploy.sh
+++ documentation/generateDocumentationAndDeploy.sh
@@ -4,7 +4,7 @@ set -v
 echo "Starting execution, GH_REPO_SLUG = $TRAVIS_REPO_SLUG, GH_REPO_TOKEN = $GH_REPO_TOKEN"
 GH_REPO_ORG=$(echo $TRAVIS_REPO_SLUG | cut -d "/" -f 1)
 GH_REPO_NAME=$(echo $TRAVIS_REPO_SLUG | cut -d "/" -f 2)
-GH_REPO_REF=${TRAVIS_REPO_SLUG}
+GH_REPO_REF=github.com/${TRAVIS_REPO_SLUG}
 ################################################################################
 # Title         : generateDocumentationAndDeploy.sh
 # Date created  : 2017/04/22
@@ -50,7 +50,7 @@ mkdir code_docs
 cd code_docs
 
 # Get the current gh-pages branch
-git clone -b gh-pages https://github.com/${GH_REPO_REF}.git
+git clone -b gh-pages https://${GH_REPO_REF}.git
 cd $GH_REPO_NAME
 
 ##### Configure git.
@@ -104,7 +104,7 @@ if [ -d "html" ] && [ -f "html/index.html" ]; then
     # Force push to the remote gh-pages branch.
     # The ouput is redirected to /dev/null to hide any sensitive credential data
     # that might otherwise be exposed.
-    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" # > /dev/null 2>&1
 else
     echo '' >&2
     echo 'Warning: No documentation (html) files have been found!' >&2
