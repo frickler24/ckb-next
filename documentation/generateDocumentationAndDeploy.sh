9f984974b40c50530f8f2ba3d827ac04442eb596
Lutz Eichler <github@frickler24.de>
2017-04-23 23:48:06 +0200

    Use new path for html-file checking at the end

--- documentation/generateDocumentationAndDeploy.sh
+++ documentation/generateDocumentationAndDeploy.sh
@@ -121,7 +121,7 @@ echo done generating.
 # Only upload if Doxygen successfully created the documentation.
 # Check this by verifying that the html directory and the file html/index.html
 # both exist. This is a good indication that Doxygen did it's work.
-if [ -d "html" ] && [ -f "html/index.html" ]; then
+if [ -d "${DOXDIR}/all/html" ] && [ -f "${DOXDIR}/all/html/index.html" ]; then
 
     echo 'Uploading documentation to the gh-pages branch...'
     # Add everything in this directory (the Doxygen code documentation) to the
