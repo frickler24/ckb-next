c658f72b62793b23764bf6e79b2505f49db53fc0
Lutz Eichler <github@frickler24.de>
2017-05-22 13:24:50 +0200

    Add doxygen and travis-ci files

--- documentation/generateDocumentationAndDeploy.sh
+++ documentation/generateDocumentationAndDeploy.sh
@@ -117,10 +117,12 @@ rm -f .nojekyll
 echo doxygen done.
 
 echo Generating pdf from latex directories in $(pwd)
-(cd ${DOXDIR}/all/latex ; make -f ../../../documentation/Makefile_skeleton > make_pdf1.log ) &
+cp ../../documentation/Makefile_skeleton ${DOXDIR}/all/latex/Makefile
+(cd ${DOXDIR}/all/latex ; make > make_pdf1.log ) &
 # echo Generating pdf from latex2
-(cd ${DOXDIR}/ckb/latex ; make -f ../../../documentation/Makefile_skeleton > make_pdf2.log ) &
-# echo Generating pdf from latex3 without special handling
+cp ../../documentation/Makefile_skeleton ${DOXDIR}/ckb/latex/Makefile
+(cd ${DOXDIR}/ckb/latex ; make > make_pdf2.log ) &
+# echo Generating pdf from latex3
 (cd ${DOXDIR}/ckb-daemon/latex ; make > make_pdf3.log ) &
 wait
 echo done generating.
